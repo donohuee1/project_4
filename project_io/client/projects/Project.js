1353b8335a803e87c7d6413b02ef23093efdf8b8
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-04-12 16:23:05 -0700

    updates edit button to only open on specific project that was clicked

--- project_io/client/projects/Project.js
+++ project_io/client/projects/Project.js
@@ -1,6 +1,16 @@
+// Use Reactive var package
+Template.Project.onCreated(function() {
+  //When template is created, set a new reactive variable to false
+  this.editMode = new ReactiveVar(false);
+});
+
 Template.Project.helpers({
   updateProjectId: function() {
     return this._id;
+  },
+  editMode: function() {
+    //only return this specific templates editMode reactive variable.
+    return Template.instance().editMode.get();
   }
 });
 
@@ -11,8 +21,11 @@ Template.Project.events({
   'click .fa-trash': function() {
     Meteor.call('deleteProject', this._id);
   },
-  'click .fa-pencil': function() {
+  //need access to this specific template and need to acccess this.editMode reactive var.  Modify this.editMode:
+  'click .fa-pencil': function(event, template) {
+    //When fa-pencil is clicked, it will reset editMode to the opposite of false: true
+    template.editMode.set(!template.editMode.get());
     // edit mode = to opposite of what session.get of what editMode is
-    Session.set('editMode', !Session.get('editMode'));
+    // Session.set('editMode', !Session.get('editMode'));
   }
 });
