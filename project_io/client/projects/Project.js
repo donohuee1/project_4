7db188277950a00f2413cc03cbb41671658f6772
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-04-12 14:41:16 -0700

    adds edit function to tasks in project book view

--- project_io/client/projects/Project.js
+++ project_io/client/projects/Project.js
@@ -1,3 +1,9 @@
+Template.Project.helpers({
+  updateProjectId: function() {
+    return this._id;
+  }
+});
+
 Template.Project.events({
   'click .toggle-gantt': function() {
     Meteor.call('toggleGanttTask', this._id, this.inGantt)
