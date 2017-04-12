bae83285d7d47483317b5d7170f7e54517a45ebb
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-04-11 17:48:47 -0700

    template subscription

--- project_io/client/projects/Projects.js
+++ project_io/client/projects/Projects.js
@@ -1,4 +1,13 @@
-Meteor.subscribe('projects');
+// Meteor.subscribe('projects');
+
+//subscribe to projects at the template level. So if we want to reuse the projeccts temmplate anywhere else, it's not tied to the routes, just tied to the template itself. 
+Template.Projects.onCreated(function() {
+  var self = this;
+  //unsubscribe us from any old subscriptions. When we go a different project, we're not still subscribed to the old one.
+  self.autorun(function() {
+    self.subscribe('projects');
+  });
+});
 
 Template.Projects.helpers({
   projects: ()=> {
