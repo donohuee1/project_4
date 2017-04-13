ff15c42072798e1a3d2aef987daac3eee105ec72
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-04-12 17:10:51 -0700

    adds new project button toggle for new project form

--- project_io/client/projects/Projects.js
+++ project_io/client/projects/Projects.js
@@ -1,6 +1,6 @@
 // Meteor.subscribe('projects');
 
-//subscribe to projects at the template level. So if we want to reuse the projeccts temmplate anywhere else, it's not tied to the routes, just tied to the template itself. 
+//subscribe to projects at the template level. So if we want to reuse the projeccts temmplate anywhere else, it's not tied to the routes, just tied to the template itself.
 Template.Projects.onCreated(function() {
   var self = this;
   //unsubscribe us from any old subscriptions. When we go a different project, we're not still subscribed to the old one.
@@ -12,5 +12,12 @@ Template.Projects.onCreated(function() {
 Template.Projects.helpers({
   projects: ()=> {
     return Projects.find({});
+  }
+});
+
+
+Template.Projects.events({
+  'click .new-project': () => {
+    Session.set('newProject', true);
   }
 });
